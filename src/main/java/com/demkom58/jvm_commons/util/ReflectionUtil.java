943798e6bde56f1b989bd5f1155498b60dc0708d
DEMKOM58 <demkom58@gmail.com>
2019-09-17 00:21:11 +0300

    Added nullable annotation to getFieldValue

--- src/main/java/com/demkom58/jvm_commons/util/ReflectionUtil.java
+++ src/main/java/com/demkom58/jvm_commons/util/ReflectionUtil.java
@@ -4,12 +4,15 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class ReflectionUtil {
 
-    public static <OWNER> Object getFieldValue(@NotNull final Class<OWNER> holder,
-                                               @NotNull final String fieldName,
-                                               @Nullable final OWNER instance) throws RuntimeException {
+    public static <OWNER> @Nullable Object getFieldValue(@NotNull final Class<OWNER> holder,
+                                                         @NotNull final String fieldName,
+                                                         @Nullable final OWNER instance) throws RuntimeException {
         try {
             Field field = holder.getDeclaredField(fieldName);
             field.setAccessible(true);
